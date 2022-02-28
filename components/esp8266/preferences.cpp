169c5da9e399adedd0baeb600e9459315500fe9f
Kaufman Home Automation <brian@kaufha.com>
2022-02-28 11:01:49 -0700

    v1.8 upload

--- components/esp8266/preferences.cpp
+++ components/esp8266/preferences.cpp
@@ -181,6 +181,9 @@ class ESP8266Preferences : public ESPPreferences {
 
       uint32_t start;
 
+      uint32_t table_end = 75;
+
+
       // ESPHome just assigns addresses as they pop up, but we want to preserve all addresses so they always remain
       // the same after an update and then saved values are always loaded properly after update.  Address is based on
       // type/hash we receive, which will always be the same for each entity based on its name.  Except WiFi, which
@@ -194,6 +197,12 @@ class ESP8266Preferences : public ESPPreferences {
       else if ( type == 3524332562 ) { start = 64; }  // v1.6 - Boot state select
       else if ( type == 5841966    ) { start = 66; }  // v1.7 - Effect select
       else if ( type == 3755051405 ) { start = 68; }  // v1.7 - First boot Variable
+      else if ( type == 706743398  ) { start = 70; }  // v1.8 - HTTP Only Switch
+      else if ( type == 3133748577 ) { start = 72; }  // v1.8 - Default Fade Length
+      else if ( type == 4189052763 ) { start = 74; }  // v1.8 - Max Power
+
+      // temporary workaround for always using address 0 for wifi even with random hash
+      else if ( length_words == 25 ) { start = 0;  }
 
       // should never get to this because ESPHome only saves a pretty small number of well defined things.
       // end users might see it if they add their own entities and also have uart logging hooked up to see messages this early.
@@ -201,7 +210,7 @@ class ESP8266Preferences : public ESPPreferences {
         ESP_LOGD("KAUF Preferences", "              !!!! NOTE: STORING PAST DEFAULT FLASH TABLE !!!!");
 
         // set start to end of memory map above if it's not there already.
-        if ( this->current_flash_offset < 70 ) {this->current_flash_offset = 70;}
+        if ( this->current_flash_offset <= table_end ) {this->current_flash_offset = table_end + 1;}
         start = this->current_flash_offset;
       }
 
@@ -218,7 +227,7 @@ class ESP8266Preferences : public ESPPreferences {
 
       // don't adopt end as current_flash_offset unless past end of memory map.
       // otherwise it will reset to 104 every time an expected type comes through.
-      if ( end > 70 ) {current_flash_offset = end;}
+      if ( end > table_end ) {current_flash_offset = end;}
 
       ESP_LOGD(TAG, "Making Preference in Flash - start: %u: length: %u, length_words:%u type: %u", start, length, length_words, type);
       return {pref};
