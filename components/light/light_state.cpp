fdac71d93603b80f63733dae27d54caa88991401
Kaufman Home Automation <brian@kaufha.com>
2022-06-08 09:42:09 -0700

    remove hash base

--- components/light/light_state.cpp
+++ components/light/light_state.cpp
@@ -285,7 +285,6 @@ bool LightState::parse_frame_(const uint8_t *payload, uint16_t size) {
 }
 
 float LightState::get_setup_priority() const { return setup_priority::HARDWARE - 1.0f; }
-uint32_t LightState::hash_base() { return 1114400283; }
 
 void LightState::publish_state() { this->remote_values_callback_.call(); }
 
