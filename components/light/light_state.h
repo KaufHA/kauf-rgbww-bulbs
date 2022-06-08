18b4a618aab6c29a388f0f7dc05258a669e72ec4
Kaufman Home Automation <brian@kaufha.com>
2022-06-08 09:42:31 -0700

    remove hash base

--- components/light/light_state.h
+++ components/light/light_state.h
@@ -180,8 +180,6 @@ protected:
   friend LightCall;
   friend class AddressableLight;
 
-  uint32_t hash_base() override;
-
   /// Internal method to start an effect with the given index
   void start_effect_(uint32_t effect_index);
   /// Internal method to get the currently active effect
